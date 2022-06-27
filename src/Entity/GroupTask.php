f05cba42cf391a2aef7d62daf5736f809f290a79
magusin <fanatsy4@gmail.com>
2022-06-27 19:57:32 +0200

    add foreign key task

--- src/Entity/GroupTask.php
+++ src/Entity/GroupTask.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\GroupTaskRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: GroupTaskRepository::class)]
@@ -13,8 +15,46 @@ class GroupTask
     #[ORM\Column(type: 'integer')]
     private $id;
 
+    #[ORM\OneToMany(mappedBy: 'group_task', targetEntity: Task::class, orphanRemoval: true)]
+    private $tasks;
+
+    public function __construct()
+    {
+        $this->tasks = new ArrayCollection();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    /**
+     * @return Collection<int, Task>
+     */
+    public function getTasks(): Collection
+    {
+        return $this->tasks;
+    }
+
+    public function addTask(Task $task): self
+    {
+        if (!$this->tasks->contains($task)) {
+            $this->tasks[] = $task;
+            $task->setGroupTask($this);
+        }
+
+        return $this;
+    }
+
+    public function removeTask(Task $task): self
+    {
+        if ($this->tasks->removeElement($task)) {
+            // set the owning side to null (unless already changed)
+            if ($task->getGroupTask() === $this) {
+                $task->setGroupTask(null);
+            }
+        }
+
+        return $this;
     }
 }
--- src/Entity/TaskStatus.php
+++ src/Entity/TaskStatus.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\TaskStatusRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: TaskStatusRepository::class)]
@@ -15,6 +17,14 @@ class TaskStatus
 
     #[ORM\Column(type: 'string', length: 45)]
     private $label;
+
+    #[ORM\OneToMany(mappedBy: 'task_status', targetEntity: Task::class, orphanRemoval: true)]
+    private $tasks;
+
+    public function __construct()
+    {
+        $this->tasks = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -29,6 +39,36 @@ class TaskStatus
     public function setLabel(string $label): self
     {
         $this->label = $label;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Task>
+     */
+    public function getTasks(): Collection
+    {
+        return $this->tasks;
+    }
+
+    public function addTask(Task $task): self
+    {
+        if (!$this->tasks->contains($task)) {
+            $this->tasks[] = $task;
+            $task->setTaskStatus($this);
+        }
+
+        return $this;
+    }
+
+    public function removeTask(Task $task): self
+    {
+        if ($this->tasks->removeElement($task)) {
+            // set the owning side to null (unless already changed)
+            if ($task->getTaskStatus() === $this) {
+                $task->setTaskStatus(null);
+            }
+        }
 
         return $this;
     }
