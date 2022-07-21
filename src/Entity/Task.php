483128c454a030145402f5917aa8ece7954cd745
magusin <fanatsy4@gmail.com>
2022-07-21 17:31:13 +0200

    taskController

--- src/Entity/Task.php
+++ src/Entity/Task.php
@@ -11,53 +11,53 @@ class Task
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
-    #[Groups(['task', 'task_details'])] 
+    #[Groups(['task'])] 
     #[ORM\Column(type: 'integer')]
     private $id;
 
-    #[Groups(['task', 'task_details'])] 
+    #[Groups(['task'])] 
     #[ORM\Column(type: 'string', length: 45)]
     private $name;
 
-    #[Groups(['task', 'task_details'])] 
+    #[Groups(['task'])] 
     #[ORM\Column(type: 'datetime')]
     private $created_at;
 
-    #[Groups(['task', 'task_details'])] 
+    #[Groups(['task'])] 
     #[ORM\Column(type: 'string', length: 45)]
     private $created_by;
 
-    #[Groups(['task', 'task_details'])] 
+    #[Groups(['task'])] 
     #[ORM\Column(type: 'datetime', nullable: true)]
     private $updated_at;
 
-    #[Groups(['task', 'task_details'])] 
+    #[Groups(['task'])] 
     #[ORM\Column(type: 'string', length: 45, nullable: true)]
     private $updated_by;
 
-    #[Groups(['task', 'task_details'])] 
+    #[Groups(['task'])] 
     #[ORM\Column(type: 'datetime', nullable: true)]
     private $deleted_at;
 
-    #[Groups(['task', 'task_details'])] 
+    #[Groups(['task'])] 
     #[ORM\Column(type: 'string', length: 45, nullable: true)]
     private $deleted_by;
 
-    #[Groups(['task_details'])] 
+    #[Groups(['project'])] 
     #[ORM\ManyToOne(targetEntity: Project::class, inversedBy: 'tasks')]
     #[ORM\JoinColumn(nullable: false)]
     private $project;
 
-    #[Groups(['task_details'])] 
+    #[Groups(['user'])] 
     #[ORM\ManyToOne(targetEntity: User::class, inversedBy: 'tasks')]
     private $user;
 
-    #[Groups(['task_details'])] 
+    #[Groups(['group_task'])] 
     #[ORM\ManyToOne(targetEntity: GroupTask::class, inversedBy: 'tasks')]
     #[ORM\JoinColumn(nullable: false)]
     private $group_task;
 
-    #[Groups(['task_details'])] 
+    #[Groups(['task_status'])] 
     #[ORM\ManyToOne(targetEntity: TaskStatus::class, inversedBy: 'tasks')]
     #[ORM\JoinColumn(nullable: false)]
     private $task_status;
