9932d7ed1498012dc592119a7ba50a793ac83f57
Wilgenbus <robinwilgenbus@gmail.com>
2022-08-16 17:00:28 +0200

    Resolve conflict

--- src/Entity/UserStory.php
+++ src/Entity/UserStory.php
@@ -67,6 +67,10 @@ class UserStory
     #[ORM\OneToMany(mappedBy: 'user_story', targetEntity: Task::class, orphanRemoval: true)]
     private $tasks;
 
+    #[Groups(['userStory_user'])] 
+    #[ORM\ManyToOne(targetEntity: User::class, inversedBy: 'user_stories')]
+    private $user;
+
     public function __construct()
     {
         $this->sprints = new ArrayCollection();
@@ -248,6 +252,18 @@ class UserStory
                 $task->setUserStory(null);
             }
         }
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
 
         return $this;
     }
